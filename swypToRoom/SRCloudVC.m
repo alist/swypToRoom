a9a51d805e4cc4bf1c1b52c2f39888b6db4d7edd
Alexander List <alist@listprojects.com>
2012-02-04 21:50:47 -0500

    adding swyp button

--- swypToRoom/SRCloudVC.m
+++ swypToRoom/SRCloudVC.m
@@ -9,18 +9,45 @@
 #import "SRCloudVC.h"
 
 @implementation SRCloudVC
-@synthesize mapBG = _mapBG, swypRoomContentTV = _swypRoomContentTV;
+@synthesize mapBG = _mapBG, swypRoomContentTV = _swypRoomContentTV, swypActivateButton = _swypActivateButton;
+
+
+-(void) activateSwypButtonPressed:(id)sender{
+	[[swypWorkspaceViewController sharedSwypWorkspace] presentContentWorkspaceAtopViewController:self];
+}
 
 -(void) viewDidLoad{
 	[super viewDidLoad];
 	
 	_mapBG = [[MKMapView alloc] initWithFrame:self.view.bounds];
+	[_mapBG setAlpha:1];
 	[_mapBG showsUserLocation];
 	[_mapBG setUserTrackingMode:MKUserTrackingModeFollowWithHeading];
 	[_mapBG setAutoresizingMask:UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth];
 	[self.view addSubview:_mapBG];
 	
 	//add tableview here
+	
+	
+	_swypActivateButton	=	[UIButton buttonWithType:UIButtonTypeCustom];
+	UIImage *	swypActivateImage	=	[UIImage imageNamed:@"swypPhotosHud"];
+	[_swypActivateButton setBackgroundImage:swypActivateImage forState:UIControlStateNormal];
+
+	_swypActivateButton	=	[UIButton buttonWithType:UIButtonTypeCustom];
+    [_swypActivateButton setFrame:CGRectMake(((self.view.width)-_swypActivateButton.size.width)/2, self.view.height-_swypActivateButton.size.height, _swypActivateButton.size.width, _swypActivateButton.size.height)];
+	[_swypActivateButton setAutoresizingMask:UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleRightMargin|UIViewAutoresizingFlexibleLeftMargin];
+
+	[_swypActivateButton addTarget:self action:@selector(activateSwypButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+	
+	UISwipeGestureRecognizer *swipeUpRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(activateSwypButtonPressed:)];
+	swipeUpRecognizer.direction = UISwipeGestureRecognizerDirectionUp;
+	[_swypActivateButton addGestureRecognizer:swipeUpRecognizer];
+	
+	[self.view addSubview:_swypActivateButton];
+	
+	//SO that overlaps don't occur btw button and bottom of TVC
+	[[self swypRoomContentTV] setContentInset:UIEdgeInsetsMake(0, 0, 75, 0)];
+
 }
 
 @end
