dd40453a2d54a61506a906914f190d38e1248c66
Alexander List <alist@listprojects.com>
2012-02-04 22:24:44 -0500

    added outgoing data manager

---
+++ swypToRoom/SROutgoingDataManager.h
@@ -0,0 +1,13 @@
+//
+//  SROutgoingDataManager.h
+//  swypToRoom
+//
+//  Created by Alexander List on 2/4/12.
+//  Copyright (c) 2012 ExoMachina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SROutgoingDataManager : NSObject
+
+@end
---
+++ swypToRoom/SROutgoingDataManager.m
@@ -0,0 +1,13 @@
+//
+//  SROutgoingDataManager.m
+//  swypToRoom
+//
+//  Created by Alexander List on 2/4/12.
+//  Copyright (c) 2012 ExoMachina. All rights reserved.
+//
+
+#import "SROutgoingDataManager.h"
+
+@implementation SROutgoingDataManager
+
+@end
