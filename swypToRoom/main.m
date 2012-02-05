306ab565b89f57253333ec286e46aad5cf0e4354
Ethan Sherbondy <sherbondye@gmail.com>
2012-02-04 21:38:33 -0500

    parse added.

--- swypToRoom/main.m
+++ swypToRoom/main.m
@@ -7,11 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
 
 #import "SRAppDelegate.h"
 
 int main(int argc, char *argv[])
 {
+    [Parse setApplicationId:@"q3FiX8q6K5ajET6MFdRwSh2icYgS93TiH5jaqOpb" 
+                  clientKey:@"eZVynljiAJID7xT6lq806fpQa642kjm1r7fh8gJk"];
+    
 	@autoreleasepool {
 	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SRAppDelegate class]));
 	}
