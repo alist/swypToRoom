d037874157f606ebbddf3c6f64e70024ef990373
Alexander List <alist@listprojects.com>
2012-02-04 21:11:28 -0500

    Initial Commit

---
+++ swypToRoom/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  swypToRoom
+//
+//  Created by Alexander List on 2/4/12.
+//  Copyright (c) 2012 ExoMachina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SRAppDelegate class]));
+	}
+}
