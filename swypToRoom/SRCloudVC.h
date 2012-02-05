78a07c138f2ce55206883189ea50b28e511e5b86
Alexander List <alist@listprojects.com>
2012-02-04 21:16:17 -0500

    added initial swyp to room project

---
+++ swypToRoom/SRCloudVC.h
@@ -0,0 +1,15 @@
+//
+//  SRCloudVC.h
+//  swypToRoom
+//
+//  Created by Alexander List on 2/4/12.
+//  Copyright (c) 2012 ExoMachina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface SRCloudVC : UIViewController <UITableViewDelegate>
+@property (nonatomic,  strong) MKMapView*	map;
+
+@end
---
+++ swypToRoom/SRCloudVC.m
@@ -0,0 +1,13 @@
+//
+//  SRCloudVC.m
+//  swypToRoom
+//
+//  Created by Alexander List on 2/4/12.
+//  Copyright (c) 2012 ExoMachina. All rights reserved.
+//
+
+#import "SRCloudVC.h"
+
+@implementation SRCloudVC
+
+@end
